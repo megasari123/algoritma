8e9f68aeea2c0fb0489b4c7ff60f23a3ba57eb8c
megasari123 <megasari800@gmail.com>
2017-04-14 17:46:40 +0700

    Add files via upload

---
+++ latihan array 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (){
+	//kamus
+	string nama1,nama2,nama3,nama4,nama5,nama6;
+	//algoritma
+	cin>>nama1;
+	cin>>nama2;
+	cin>>nama3;
+	cin>>nama4;
+	cin>>nama5;
+	cin>>nama6;
+	cout<<nama1<<"-"<<nama2<<endl;
+	cout<<nama1<<"-"<<nama3<<endl;
+	cout<<nama4<<"-"<<nama6<<endl;
+	cout<<nama1<<"-"<<nama4<<endl;
+	cout<<nama1<<"-"<<nama5<<endl;
+	cout<<nama2<<"-"<<nama4<<endl;
+}
---
+++ latihan array 4.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	//kamus
+	string nama1,nama2,nama3,nama4,nama5,nama6,nama7,nama8,nama9,nama10,
+	nama11,nama12,nama13,nama14,nama15,nama16,nama17,nama18,nama19,nama20,
+	nama21,nama22,nama23,nama24,nama25,nama26,nama27,nama28,nama29,nama30;
+	cin>>nama1;
+	cin>>nama2;
+	cin>>nama3;
+	cin>>nama4;
+	cin>>nama5;
+	cin>>nama6;
+	cin>>nama7;
+	cin>>nama8;
+	cin>>nama9;
+	cin>>nama10;
+	cin>>nama12;
+	cin>>nama13;
+	cin>>nama14;
+	cin>>nama15;
+	cin>>nama16;
+	cin>>nama17;
+	cin>>nama18;
+	cin>>nama19;
+	cin>>nama20;
+	cin>>nama21;
+	cin>>nama22;
+	cin>>nama23;
+	cin>>nama24;
+	cin>>nama25;
+	cin>>nama26;
+	cin>>nama27;
+	cin>>nama28;
+	cin>>nama29;
+	cin>>nama30;
+	
+	cout<<nama1<<"-"<<nama30<<endl;
+	cout<<nama2<<"-"<<nama29<<endl;
+	cout<<nama4<<"-"<<nama27<<endl;
+	cout<<nama5<<"-"<<nama26<<endl;
+	cout<<nama6<<"-"<<nama6<<endl;
+	cout<<nama7<<"-"<<nama7<<endl;
+	cout<<nama8<<"-"<<nama8<<endl;
+	cout<<nama9<<"-"<<nama10<<endl;
+	cout<<nama10<<"-"<<nama9<<endl;
+	cout<<nama11<<"-"<<nama11<<endl;
+	cout<<nama12<<"-"<<nama12<<endl;
+	cout<<nama13<<"-"<<nama13<<endl;
+	cout<<nama14<<"-"<<nama14<<endl;
+	cout<<nama4<<"-"<<nama15<<endl;
+	cout<<nama4<<"-"<<nama16<<endl;
+	cout<<nama5<<"-"<<nama17<<endl;
+	cout<<nama5<<"-"<<nama18<<endl;
+	cout<<nama5<<"-"<<nama20<<endl;
+	cout<<nama6<<"-"<<nama19<<endl;
+	cout<<nama7<<"-"<<nama4<<endl;
+	cout<<nama8<<"-"<<nama12<<endl;
+	cout<<nama9<<"-"<<nama13<<endl;
+	cout<<nama10<<"-"<<nama14<<endl;
+	cout<<nama23<<"-"<<nama15<<endl;
+	cout<<nama30<<"-"<<nama16<<endl;
+	cout<<nama5<<"-"<<nama17<<endl;
+	cout<<nama5<<"-"<<nama18<<endl;
+	cout<<nama5<<"-"<<nama20<<endl;
+	cout<<nama8<<"-"<<nama19<<endl;
+	cout<<nama9<<"-"<<nama21<<endl;
+	
+}
