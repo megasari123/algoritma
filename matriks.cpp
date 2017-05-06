82cd696e522c979a7b7c11d68cb1d851e919c392
megasari123 <megasari800@gmail.com>
2017-05-06 12:14:22 +0700

    Add files via upload

---
+++ matriks.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int m [3] [3];
+	for (int i=0; i<3; i++)
+	{
+	cout << "isi data ke baris" << i <<"kolom" << j <<endl;
+	cin >> m[i] [j];
+    }
+}
+cout << "berikut data yang ada" << endl;
+		
+		for (int a=0; a<3; a++)
+		{
+			for (int b=0;b<3; b++)
+			cin >> m [i] [j];
+		}
+	}
+cout << m [1] [1];
+return 0;
+}
